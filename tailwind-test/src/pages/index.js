e50bfbed6cad2e917bb7323f57127cb4c03caaa9
InnocentWahome <innocentwahome@gmail.com>
2021-08-16 20:02:08 +0300

    Tailwind CSS

---
+++ tailwind-test/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/layout/Layout';
+
+
+const Index = () => (
+  <Layout>
+  <div>
+    <h1>Hello World ,,, this is the body ,, now to correctly add the layout</h1>
+    </div>  
+    </Layout>
+);
+
+export default Index;
